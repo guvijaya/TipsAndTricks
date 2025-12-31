444ca2e3669c9fe8ad4491706733207d5f0aedaf
Guru <guru.kpmus@hotmail.com>
2025-12-30 20:47:39 -0800

    Add celebration data for birthdays and weddings

---
+++ Calendar/data.js
@@ -0,0 +1,10 @@
+const celebrationData = {
+  "birthdays": [
+        {"name": "Rama", "date": "26-Mar"}
+        {"name": "Krishna", "date": "4-Sep"}
+],
+  "weddings": [    
+        {"name": "Radha Krishna", "date": "26-Feb"},
+        {"name": "Sita Rama", "date": "12-Mar"}
+  ]
+};
